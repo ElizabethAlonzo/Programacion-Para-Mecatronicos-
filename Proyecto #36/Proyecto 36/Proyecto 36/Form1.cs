e183344e0517fdb16b168c9b47019819fb1dce32
Elizabeth Alonzo <122744216+ElizabethAlonzo@users.noreply.github.com>
2023-04-24 19:27:00 -0400

    Add files via upload

---
+++ Proyecto #36/Proyecto 36/Proyecto 36/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_36
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            Point[] puntos = { new Point(30, 45), new Point(110, 75), new Point(175, 200) };
+            g.FillClosedCurve(Brushes.Aquamarine, puntos);
+
+            g.FillEllipse(Brushes.BlueViolet, new Rectangle(75, 30, 100, 25));
+
+            Point[] puntos2 = { new Point(10, 145), new Point(200, 50), new Point(100, 220) };
+            g.FillPolygon(Brushes.LightGoldenrodYellow, puntos2);
+
+            g.DrawRectangle(Pens.Black, new Rectangle(200, 100, 30, 75));
+            g.FillRectangle(Brushes.Pink, new Rectangle(200, 100, 30, 75));
+
+        }
+    }
+}
---
+++ Proyecto #37/Proyecto 37/Proyecto 37/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Proyecto_37
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BT_Uno = new System.Windows.Forms.Button();
+            this.LB_Mensaje = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BT_Uno
+            // 
+            this.BT_Uno.Location = new System.Drawing.Point(53, 48);
+            this.BT_Uno.Margin = new System.Windows.Forms.Padding(2);
+            this.BT_Uno.Name = "BT_Uno";
+            this.BT_Uno.Size = new System.Drawing.Size(56, 19);
+            this.BT_Uno.TabIndex = 0;
+            this.BT_Uno.Text = "Uno";
+            this.BT_Uno.UseVisualStyleBackColor = true;
+            this.BT_Uno.Click += new System.EventHandler(this.BT_Uno_Click);
+            // 
+            // LB_Mensaje
+            // 
+            this.LB_Mensaje.AutoSize = true;
+            this.LB_Mensaje.Location = new System.Drawing.Point(59, 80);
+            this.LB_Mensaje.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LB_Mensaje.Name = "LB_Mensaje";
+            this.LB_Mensaje.Size = new System.Drawing.Size(35, 13);
+            this.LB_Mensaje.TabIndex = 3;
+            this.LB_Mensaje.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 301);
+            this.Controls.Add(this.LB_Mensaje);
+            this.Controls.Add(this.BT_Uno);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BT_Uno;
+        private System.Windows.Forms.Label LB_Mensaje;
+    }
+}
+
